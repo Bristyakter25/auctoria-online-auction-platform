15246ee1a7c294f500cd39a0637919cdba652658
Bristyakter25 <sabihaakterbristy@gmail.com>
2025-04-09 19:05:28 +0600

    fixed google signup users storage issue

--- BidApp/src/Route/Routes.jsx
+++ BidApp/src/Route/Routes.jsx
@@ -13,8 +13,6 @@ import ManageUsers from "../Pages/Dashboard/ManageUsers";
 import WishList from "../Pages/WishList";
 import UpdateBid from "../components/sellerComponents/updateBid";
 import Bid from "../Pages/BidTask/Bid";
-import ForgotPassword from "../Pages/Authentication/ForgotPassword";
-import ResetPassword from "../Pages/Authentication/ResetPassword";
 
 export const router = createBrowserRouter([
   {
@@ -28,14 +26,7 @@ export const router = createBrowserRouter([
       { path: "/allAuctions", element: <AllAuctions /> },
       { path: "/profile", element: <Profile /> },
       { path: "/bid/:id", element: <Bid /> },
-      {
-        path: "/forgot-password",
-        element: <ForgotPassword></ForgotPassword>
-      },
-      {
-        path: "/reset-password",
-        element: <ResetPassword></ResetPassword>
-      }
+     
     ],
   },
   {
